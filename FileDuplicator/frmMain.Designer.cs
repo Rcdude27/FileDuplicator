69ba3d8e6c046d0d9451b4ce82ef73e406a711e8
Bryce Bontz <bontzbryce@gmail.com>
2019-11-13 18:52:04 -0600

    Add project files.

---
+++ FileDuplicator/frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace FileDuplicator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.btn_Duplicate = new System.Windows.Forms.Button();
+            this.ofdSource = new System.Windows.Forms.OpenFileDialog();
+            this.sfdDestination = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(13, 16);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(44, 13);
+            this.lblSource.TabIndex = 0;
+            this.lblSource.Text = "Source:";
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(13, 49);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(63, 13);
+            this.lblDestination.TabIndex = 1;
+            this.lblDestination.Text = "Destination:";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(82, 13);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(345, 20);
+            this.txtSource.TabIndex = 2;
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(82, 46);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.ReadOnly = true;
+            this.txtDestination.Size = new System.Drawing.Size(345, 20);
+            this.txtDestination.TabIndex = 3;
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(435, 10);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 25);
+            this.btnSource.TabIndex = 4;
+            this.btnSource.Text = "Browse";
+            this.btnSource.UseVisualStyleBackColor = true;
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(435, 44);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(75, 25);
+            this.btnDestination.TabIndex = 5;
+            this.btnDestination.Text = "Browse";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            // 
+            // btn_Duplicate
+            // 
+            this.btn_Duplicate.Location = new System.Drawing.Point(225, 72);
+            this.btn_Duplicate.Name = "btn_Duplicate";
+            this.btn_Duplicate.Size = new System.Drawing.Size(75, 25);
+            this.btn_Duplicate.TabIndex = 6;
+            this.btn_Duplicate.Text = "Duplicate";
+            this.btn_Duplicate.UseVisualStyleBackColor = true;
+            // 
+            // ofdSource
+            // 
+            this.ofdSource.FileName = "ofdSource";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 101);
+            this.Controls.Add(this.btn_Duplicate);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.lblSource);
+            this.Name = "frmMain";
+            this.Text = "File Duplicator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Button btn_Duplicate;
+        private System.Windows.Forms.OpenFileDialog ofdSource;
+        private System.Windows.Forms.SaveFileDialog sfdDestination;
+    }
+}
+
